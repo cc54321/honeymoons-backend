32e88e4164cf4a941a745a3dce544e56506d44aa
cc54321 <charles.cole@codeimmersives.com>
2023-08-27 17:18:10 -0400

    git push

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
-app.use(cors(
-  {
-    origin: process.env.CORS_ORIGIN
-  }
- ))
+// app.use(cors(
+//   {
+//     origin: process.env.CORS_ORIGIN
+//   }
+//  ))
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter); // Update the route
